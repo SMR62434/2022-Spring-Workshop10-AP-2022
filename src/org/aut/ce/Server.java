efed635f8e30932bb66a550481670e26f9da20e3
Mahdi <m.rohani1381@gmail.com>
2022-12-22 15:14:16 +0330

    feat : Server

--- src/org/aut/ce/Server.java
+++ src/org/aut/ce/Server.java
@@ -1,12 +1,39 @@
 package org.aut.ce;
 
 
-import java.io.IOException;
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
 
 public class Server {
 
-    public static void main(String[] args) throws IOException {
+    private final ServerSocket serverSocket;
 
+    public Server(ServerSocket serverSocket) {
+        this.serverSocket = serverSocket;
+    }
+
+    public void startServer() throws IOException {
+        try {
+            while (!serverSocket.isClosed()) {
+                Socket socket = serverSocket.accept();
+                System.out.println("a new user joined the chat");
+                ClientHandler clientHandler = new ClientHandler(socket);
+                Thread thread = new Thread(clientHandler);
+                thread.start();
+            }
+        } catch (IOException e) {
+            serverSocket.close();
+        }
+    }
+
+    public void closeServerSocket() {
+    }
+
+    public static void main(String[] args) throws IOException {
+        ServerSocket serverSocket = new ServerSocket(2000);
+        Server server = new Server(serverSocket);
+        server.startServer();
     }
 
 }
